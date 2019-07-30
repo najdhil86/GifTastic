fd3eae6147681fff811e5870a00cd2fa019f7388
Naj Dhillon <najdhil86@gmail.com>
2019-07-30 10:02:38 -0700

    Figuring out issue where button does not display giphys

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,78 @@
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=M2qGlCIrSZY1ge15FIG17uNEAg9SoxR8&q=falcon&limit=25&offset=0&rating=G&lang=en"
+// Initial array of movies
+var animals = ["cats", "dogs", "mice", "lion", "goat", "falcon"];
+  
+// Function for displaying movie data
+function makeButtons() {
 
-var animals = ['cat','dog','frog','goat','falcon'];
+  // Deleting the movie buttons prior to adding new movie buttons
+  // (this is necessary otherwise we will have repeat buttons)
+  $("#buttons-view").empty();
+
+  // Looping through the array of movies
+  for (var i = 0; i < animals.length; i++) {
+
+    var button = $("<button>");
+
+    button.addClass("animal");
+
+    button.attr("data-name", animals[i]);
+
+    button.attr("id", "animalID");
+
+    button.text(animals[i]);
+
+    $("#buttons-view").append(button);
+  }
+}
+
+$("#add-animal").on("click", function(event) {
+
+  event.preventDefault();
+
+  
+  var animal_value = $("#animal-input").val().trim();
+  
+  animals.push(animal_value);
+
+  makeButtons();
+
+});
+
+makeButtons();
+
+$(document).on('click', 'animalID', function(){
+
+  debugger;
+
+  console.log("hi");
+
+  // var animal_text = $(this).val();
+
+  // console.log(animal_text);
+
+  // var url = "https://api.giphy.com/v1/gifs/search?api_key=M2qGlCIrSZY1ge15FIG17uNEAg9SoxR8&q=" + animal_text + "&limit=25&offset=0&rating=G&lang=en";
+
+  // console.log(url);
+
+  // event.preventDefault();
 
 
-$('#find-animal-giphy').on('click', function(){
-    var v = $('#giphy-value').val();
+  // $.ajax({
 
-    var u = "https://api.giphy.com/v1/gifs/search?api_key=M2qGlCIrSZY1ge15FIG17uNEAg9SoxR8&q="+ v + "&limit=25&offset=0&rating=G&lang=en";
+  //   url: url,
+  //   method: "GET"
+  // }).then(function(response) {
 
-    console.log(u)
 
-    $.ajax({
-        url: u,
-        method: "GET"}).then(function(response) {
-            
-            for (var i=0; i<response.data.length; i++){
-                
-                var img = $('<img>'); //<img>
-                img.attr('src', response.data[i].images.fixed_height.url);
-          
-                // <img src="">
-                $('#show-animal-giphy').append(img);
-            }
-        });
+  //   console.log(response);
+    
+  //   var img = $('<img>');
 
-        var movie = $("#movie-input").val().trim();
+  //   img.attr('src', response.data[0].images.downsized_large.url)
 
-        movies.push(movie);
-        renderButtons();
+  //   $('#animal-giphys').append(img);
+  //   debugger;
+  // });
 
-        event.preventDefault();
-      });
 
-      function renderButtons() {
-
-        
-        $("#buttons-view").empty();
-
-        
-        for (var j = 0; j < animals.length; j++) {
-
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adding a class
-          a.addClass("animals");
-          // Adding a data-attribute with a value of the movie at index i
-          a.attr("data-name", animals[j]);
-          // Providing the button's text with a value of the movie at index i
-          a.text(animals[j]);
-          // Adding the button to the HTML
-          $("#buttons-view").append(a);
-        }
-      }
-
-      // This function handles events where one button is clicked
-      $("#add-movie").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
-
-        // This line will grab the text from the input box
-        
-        // The movie from the textbox is then added to our array
-        movies.push(movie);
-
-        // calling renderButtons which handles the processing of our movie array
-        
-      });
+})
